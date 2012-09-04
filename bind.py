50dee20c59e101ff331e2b3548e674a839b13977
inkerra <inkerra@gmail.com>
2012-09-04 21:38:47 +0400

    cosmetic

--- bind.py
+++ bind.py
@@ -6,14 +6,15 @@ import functools
 def func(x, y, z, t):
     return x, y, z, t
 
-def bind(f, *pending_vals, **pending_params):
-    @functools.wraps(f)
+def bind(function, *pending_vals, **pending_params):
+    @functools.wraps(function)
     def closure(*vals, **params):
         vals = pending_vals + vals
         params.update(pending_params)
-        res = f(*vals, **params)
+        res = function(*vals, **params)
         return res
     return closure
 
-f1 = bind(func, 1, 2, t=13)
-assert f1([4]) == (1, 2, [4], 13)
+if __name__ == "__main__":
+	f1 = bind(func, 1, 2, t=13)
+	assert f1([4]) == (1, 2, [4], 13)
