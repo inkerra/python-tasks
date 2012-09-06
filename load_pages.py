c1b2a47170b900622566688ce9447c509d8c932a
inkerra <inkerra@gmail.com>
2012-09-06 15:52:35 +0400

    cosmetic

--- load_pages.py
+++ load_pages.py
@@ -5,24 +5,24 @@ import urllib2
 import Queue
 import threading
 
-def get_content(url):
+def __get_content(url):
     try:
         return urllib2.urlopen(url).read()
     except Exception as e:
-        print "Warning: can't open {}".format(url)
-        return ""
+        return None
     
-def loader(param_q, res_q, func):
+def __loader(param_q, res_q, func):
     while 1:
         param = param_q.get()
         if param is None: break
         res_q.put((param, func(param)))
 
 def load_pages(max_thread_pool_sz, *urls):
+    """ generator of loaded pages """
     param_q = Queue.Queue()
     res_q = Queue.Queue()
 
-    loader_params = (param_q, res_q, get_content)
+    loader_params = (param_q, res_q, __get_content)
     loaders = []
 
     for u in urls:
@@ -32,7 +32,7 @@ def load_pages(max_thread_pool_sz, *urls):
         param_q.put(None)
 
     for i in range(max_thread_pool_sz):
-        th = threading.Thread(None, loader, "loader-{}".format(i), loader_params)
+        th = threading.Thread(None, __loader, "loader-{}".format(i), loader_params)
         th.daemon = True
         th.start()
         loaders.append(th)
@@ -45,14 +45,16 @@ def load_pages(max_thread_pool_sz, *urls):
         th.join()
 
 if __name__ == "__main__":
-    urls = ["https://google.com", "http://localhost", \
+    urls = [ \
+    "https://google.com", \
+    "http://localhost", \
     "http://inkerrapp.appspot.com/?act=show&key=agtzfmlua2VycmFwcHImCxIEQmxvZyIMZGVmYXVsdF9ibG9nDAsSCEJsb2dQb3N0GLHqAQw",
     "http://inkerrapp.appspot.com/?act=show&key=agtzfmlua2VycmFwcHIlCxIEQmxvZyIMZGVmYXVsdF9ibG9nDAsSCEJsb2dQb3N0GPIuDA",
     "incorrect_url",
     ]
 
-    for url, page in load_pages(5, *urls):
-        try:
+    for url, page in load_pages(2, *urls):
+        if page is not None:
             print url, len(page)
-        except:
-            pass
+        else:
+            print "ERROR: can't open {}".format(url)
