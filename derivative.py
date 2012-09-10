c15b781c4d3bd07f2718d569855db9d6610c6ddb
inkerra <inkerra@gmail.com>
2012-09-10 08:57:32 +0400

    cosmetic

--- derivative.py
+++ derivative.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 
 from types import FunctionType
+from operator import *
 
-ADD = lambda x, y: x + y
-SUB = lambda x, y: x - y
-MUL = lambda x, y: x * y
-POW = lambda x, y: x ** y
-DIV = lambda x, y: float(x) / y if y and x % y != 0 else x / y
-FLOORDIV = lambda x, y: x // y
-NEG = lambda x: -x
+# supported operators:
+ADD = lambda x, y: __try_calc(add, x, y)
+SUB = lambda x, y: __try_calc(sub, x, y)
+MUL = lambda x, y: __try_calc(mul, x, y)
+POW = lambda x, y: __try_calc(pow, x, y)
+DIV = lambda x, y: __try_calc(div, __try_calc(float, x), y)
+FLOORDIV = lambda x, y: __try_calc(floordiv, x, y)
+NEG = lambda x: __try_calc(neg, x)
 
-__is_binary_op = lambda f: f.func_code.co_argcount == 2
+is_binary_op = lambda f: f.func_code.co_argcount == 2
 
 class ExpressionError(Exception):
     pass
@@ -24,7 +26,7 @@ def tree_gen(node):
     if node:
         if isinstance(node, Expression):
 	    if node.op is not None:
-		if __is_binary_op(node.op):
+		if is_binary_op(node.op):
 			yield node.op
 	    	else:
 		    yield node.op
@@ -38,39 +40,33 @@ def tree_gen(node):
 def calculate(exp, **assigned_variables):
     """ calculate value of Expression exp,
 	assigning variables from assigned_variables dictionary """
-    for res in __calc(tree_gen(exp), **assigned_variables):
+    for res in calc_gen(tree_gen(exp), **assigned_variables):
     	if res is None:
 		raise ExpressionError("all variables should be assigned")
 	return res
 	
-def __calc(it, **kwargs):
+def calc_gen(it, **kwargs):
     for i in it:
 	if isinstance(i, FunctionType):
 	    lf = None
-            for _ in __calc(it, **kwargs):
+            for _ in calc_gen(it, **kwargs):
                 lf = _
 		break
-            lv = __val(lf, **kwargs)
-            if not __is_binary_op(i):
-                yield __try_calc(i, lv)
+            lv = val(lf, **kwargs)
+            if not is_binary_op(i):
+                yield i(lv)
 		return
 
 	    rg = None
-	    for _ in __calc(it, **kwargs):
+	    for _ in calc_gen(it, **kwargs):
 	        rg = _
 		break
-	    rv = __val(rg, **kwargs)
-	    yield __try_calc(i, lv, rv)
+	    rv = val(rg, **kwargs)
+	    yield i(lv, rv)
 	else: # variable or number
-	    yield __val(i, **kwargs)
+	    yield val(i, **kwargs)
 
-def __val(exp, **kwargs):
-    if isinstance(exp, str): #Var
-	if kwargs.has_key(exp):
-		return kwargs[exp]
-	return None
-    else: #calculated value
-	return exp
+val = lambda exp, **kwargs: kwargs.get(exp, None) if isinstance(exp, str) else exp
 
 def __try_calc(func, *args):
     return None if reduce(lambda x, y: x or y is None, args, False) else func(*args)
@@ -134,48 +130,45 @@ def derivative(exp, dvar, **assigned_variables):
     """ calculate derivative of Expression exp,
 	with respect to variable which name is dvar,
 	assigning variables from assigned_variables dictionary """
-    for res in __der_calc(tree_gen(exp), dvar, **assigned_variables):
+    for res in d_calc_gen(tree_gen(exp), dvar, **assigned_variables):
 	if res is None or res[1] is None:
 		raise ExpressionError("unassigned variables in result expression")
         return res[1]
 
-def __der_calc(it, dvar, **kwargs):
+def d_calc_gen(it, dvar, **kwargs):
     for i in it:
 	if isinstance(i, FunctionType):
 	    lf = None
-	    for _ in __der_calc(it, dvar, **kwargs):
+	    for _ in d_calc_gen(it, dvar, **kwargs):
 		lf, dlf = _
 		break
-	    lv, dlv = __val(lf, **kwargs), __val(dlf, **kwargs)
-	    if not __is_binary_op(i):
-		yield __try_calc(i, lf), __try_calc(i, dlf)
+	    lv, dlv = val(lf, **kwargs), val(dlf, **kwargs)
+	    if not is_binary_op(i):
+		yield i(lf), i(dlf)
 		return
-	    for _ in __der_calc(it, dvar, **kwargs):
+	    for _ in d_calc_gen(it, dvar, **kwargs):
 		rg, drg = _
 		break
 
-	    rv, drv = __val(rg, **kwargs), __val(drg, **kwargs)
-	    iv, idv = __try_calc(i, lv, rv), __try_calc(i, dlv, drv)
+	    rv, drv = val(rg, **kwargs), val(drg, **kwargs)
+	    iv, idv = i(lv, rv), i(dlv, drv)
 
 	    if i == MUL:
-		lprod, rprod = 0 if drv == 0 else __try_calc(MUL, lv, drv), \
-				0 if dlv == 0 else __try_calc(MUL, rv, dlv)
-		yield (iv, __try_calc(ADD, lprod, rprod))
+		lprod, rprod = 0 if drv == 0 else MUL(lv, drv), \
+				0 if dlv == 0 else MUL(rv, dlv)
+		yield (iv, ADD(lprod, rprod))
 	    elif i == POW:
-		yield (iv, __try_calc(MUL, dlv, \
-			__try_calc(MUL, rv, __try_calc(POW, lv, (rv - 1)))))
+		yield (iv, MUL(dlv, MUL(rv, POW(lv, (rv - 1)))))
 	    elif i == DIV:
-		yield (iv, \
-			__try_calc(DIV, \
-				__try_calc(MUL, dlv, rv) - \
-				__try_calc(MUL, lv, drv), \
-					__try_calc(POW, float(rv), 2)) )
+		yield (iv, DIV(MUL(dlv, rv) - MUL(lv, drv), POW(float(rv), 2)))
 	    elif i in {ADD, SUB}:
 		yield (iv, idv)
 	    else:
 		raise ExpressionError("unsupported operation for derivation")
 	else:
-            yield __val(i, **kwargs), (i == dvar) if isinstance(i, str) else 0
+            yield val(i, **kwargs), (i == dvar) if isinstance(i, str) else 0
+
+__all__ = ["Var", "Expression", "ExpressionError", "derivative"]
 
 if __name__ == "__main__":
     assert 1 == derivative(Var('x'), 'x')
@@ -200,6 +193,6 @@ if __name__ == "__main__":
     assert (-1) == (- Var('x') ** Var('x') + Var('y'))(x=2, y=3)
     assert 6 * 2 ** 5 == derivative(Var('x') ** 6, 'x', x=2)
     assert (-3) == derivative((2 * Var('x') + 1) / (Var('x') - 1), 'x', x=2)
-    assert (-1.5) == (- Var('x') / 2)(x=3)
-    assert (-0.25) ==  derivative(1 / Var('x'), 'x', x=2)
+    assert abs(1.5 + (- Var('x') / 2)(x=3)) < 1E-7
+    assert abs(0.25 + derivative(1 / Var('x'), 'x', x=2)) < 1E-7
     assert 21 ==  derivative((Var('x') ** 2 + 1) * (Var('x') + 2), 'x', x=2)
