ad30b5255f085ea2c54e73dd9a7ef12eb8800f31
inkerra <inkerra@inkerra-desktop>
2012-09-13 01:31:32 +0400

    fixed aspect_python

--- aspect_python.py
+++ aspect_python.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-#import sys
+import sys
 #from importlib import import_module
 #import glob
 import imp
 import inspect
 import re
-import sys
 
 DEBUG_MODE = 1
 
@@ -21,19 +20,27 @@ def source_file(f):
 
 def import_module_by_path(filepath):
     mod_name, file_ext = os.path.splitext(os.path.split(filepath)[-1])
-    try:
-        if sys.modules.has_key(mod_name) and source_file(sys.modules[mod_name].__file__) == source_file(filepath):
-            dbg("skipped (already imported) {} from {}".format(mod_name, filepath))
+    if file_ext == ".py":
+        dbg("importing {} from {}".format(mod_name, filepath))
+        if sys.modules.has_key(mod_name): del sys.modules[mod_name]
+        try:
+            return imp.load_source(mod_name, filepath)
+        except:
+            pass
+    elif re.match("\.py[ocd]|\.so", file_ext):
+        if os.path.exists(source_file(filepath)):
+            dbg("skipped (source file exists) {} from {}".format(mod_name, filepath))
             return
 
-        if file_ext == ".py":
-            dbg("importing {} from {}".format(mod_name, filepath))
-            return imp.load_source(mod_name, filepath)
-        elif re.match("\.py[ocd]|\.so", file_ext):
-            dbg("importing (compiled) {} from {}".format(mod_name, filepath))
+        if sys.modules.has_key(mod_name): del sys.modules[mod_name]
+        dbg("importing (compiled) {} from {}".format(mod_name, filepath))
+        try:
             return imp.load_compiled(mod_name, filepath)
-    except:
-        dbg("skipped (can't load) {}".format(filepath))
+        except:
+            pass
+    else:
+        return
+    dbg("skipped (can't load) {}".format(filepath))
 
 def aspect_python(directory, regexp):
     for (path, dirs, files) in os.walk(directory):
@@ -45,8 +52,13 @@ def aspect_python(directory, regexp):
                     if  inspect.isclass(obj) and re.match(regexp, obj.__name__):
                         yield obj
 
+__all__ = ['aspect_python']
+
 if __name__ == "__main__":
     for d in aspect_python("plugins", r'D\d\d\d'):
-        print "aspect:", d()
+        print "D\d\d\d:", d()
     for cls in aspect_python("plugins", r'MyNewO.*'):
         cls().action(2)
+    for cls in aspect_python("plugins", r'MyNewA.*'):
+        cls().action()
+    for d in aspect_python("plugins", r'D1\d\d'): print "D1\d\d:", d()
