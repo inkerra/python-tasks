fec9483ece5e1c2a2d4afff096b6c4f2275816bb
inkerra <inkerra@inkerra-desktop>
2012-09-12 23:39:50 +0400

    aspect_python improved

--- aspect_python.py
+++ aspect_python.py
@@ -9,24 +9,26 @@ import imp
 import inspect
 import re
 
+def import_module_by_path(filepath):
+    mod_name, file_ext = os.path.splitext(os.path.split(filepath)[-1])
+    try:
+        if file_ext == "py":
+            return imp.load_source(mod_name, filepath)
+        elif re.match("py[ocd]|so", file_ext):
+            return imp.load_compiled(mod_name, filepath)
+    except:
+        print "Skipped {}".format(filepath)
+
 def aspect_python(directory, regexp):
-    res = []
     for (path, dirs, files) in os.walk(directory):
         for f in files:
-            if f.endswith(".py"):
-                filepath = os.path.join(path, f) 
-                mod_name, file_ext = os.path.splitext(os.path.split(filepath)[-1])
-                try:
-                    mod_obj = imp.load_source(mod_name, filepath)
-                except:
-                    print "Warning: can't load {} from {}, skipped".format(mod_name, filepath)
-                    continue
-
+            mod_obj = import_module_by_path(os.path.join(path, f))
+            if mod_obj:
                 for elem in dir (mod_obj):
                     obj = getattr(mod_obj, elem)
                     if  inspect.isclass(obj) and re.match(regexp, obj.__name__):
-                            res.append(obj)
-    return res
+                        yield obj
+
 if __name__ == "__main__":
     for d in aspect_python("plugins", r'D\d\d\d'):
         print "aspect:", d()
